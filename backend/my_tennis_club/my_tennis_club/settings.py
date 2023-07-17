2b794fbddce4501371854934e09b0cf6465502c9
Kazuki Ochiai <kazu910739@gmail.com>
2023-07-17 12:22:32 +0900

    Create superuser and set member display

--- backend/my_tennis_club/my_tennis_club/settings.py
+++ backend/my_tennis_club/my_tennis_club/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-h=l+u&cnfkag6b3hm%!#hvhlfvb%)9*opxk0-aw$=1@rwc3uo8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
