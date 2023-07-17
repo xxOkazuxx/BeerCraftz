82b70e7b2a27de2c172cba82e69e01b87ea1113a
Kazuki Ochiai <kazu910739@gmail.com>
2023-07-17 09:42:49 +0900

    Add 404 page and testing page

--- backend/my_tennis_club/members/urls.py
+++ backend/my_tennis_club/members/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 
 urlpatterns = [
+    path('', views.main, name='main'),
     path('members/', views.members, name='members'),
     path('members/details/<int:id>', views.details, name='details'),
+    path('testing/', views.testing, name='testing'),
 ]
